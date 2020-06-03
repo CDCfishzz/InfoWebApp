376d16f7efe0022e6d3ee53f88404f3dcba39dd3
CDCfishzz <cdcfishzz@gmail.com>
2020-06-03 15:07:19 +0800

    2020.06.03

--- drag.js
+++ drag.js
@@ -8,36 +8,64 @@ document.ondrop = function (e) {e.preventDefault();}
 function drag(obj) {
 	obj.addEventListener('dragstart', function (ev) {
 		dragElement = this;
-		this.style.backgroundColor = '#ddd';
 	}, false);
 
 	obj.addEventListener('dragend', function (ev) {
 		ev.target.style.backgroundColor = '#fff';
 		ev.preventDefault();
+
+		// exchange both postion
+		if (enterElement) {
+			enterElement.style.backgroundColor = '#fff';
+			tamp = this.cloneNode(true);
+			this.parentNode.insertBefore(tamp, this);
+			this.parentNode.insertBefore(this, enterElement);
+			this.parentNode.replaceChild(enterElement, tamp);
+			tamp = null;
+
+			// storage change
+			storageChange(this.parentNode.childNodes);
+		}
+
+
+		enterElement = null;
+		dragElement = null;
 	}, false)
 
 	obj.addEventListener('dragenter', function (ev) {
 		nextchild = this.nextElementSibling || this.nextSilbing;
 		lastchild = this.parentNode.lastElementChild || this.parentNode.lastChild;
-		if (Idx(dragElement) > Idx(this)) {
-			this.parentNode.insertBefore(dragElement, this);
-		}else{
-			this.parentNode.insertBefore(dragElement, nextchild);
+
+		if (enterElement) {
+			enterElement.style.backgroundColor = '#fff';
 		}
-		if (lastchild == this) {
-			this.parentNode.appendChild(dragElement);
-		}
+
+		enterElement = this;
+		this.style.backgroundColor = '#ddd';
+
+		// if (Idx(dragElement) > Idx(this)) {
+		// 	this.parentNode.insertBefore(dragElement, this);
+		// }else{
+		// 	this.parentNode.insertBefore(dragElement, nextchild);
+		// }
+		// if (lastchild == this) {
+		// 	this.parentNode.appendChild(dragElement);
+		// }
+
+
 	}, false)
 
 	// obj.addEventListener('dragleave', function (ev) {
-	// 	if (dragElement != this) {
-	// 		if (lock && (this == this.parentNode.lastElementChild || this == this.parentNode.lastChild)) {
-	// 			this.parentNode.appendChild(dragElement);
-	// 			lock = false;
-	// 		}else{
-	// 			lock = true;
-	// 		}
-	// 	}
+		// if (dragElement != this) {
+		// 	if (this == this.parentNode.lastElementChild || this == this.parentNode.lastChild) {
+		// 		// this.parentNode.appendChild(dragElement);
+		// 		lock = false;
+		// 	}else{
+		// 		lock = true;
+		// 	}
+		// }
+		// enterElement = null;
+		// this.style.backgroundColor = '#fff';
 	// }, false)
 }
 
